fdfa02f8038ac90e72bb8755e8b1f2f0c5bc520b
Manish Chavan <manishc1@umbc.edu>
2014-06-08 23:22:39 -0400

    Syntactic changes.

--- src/utils/xml_utils.py
+++ src/utils/xml_utils.py
@@ -5,17 +5,17 @@ Basic xml functional utilities.
 from lxml import etree
 import lxml.builder as lb
 
-XML_HEAD = '<?xml version="1.0" encoding="UTF-8"?>\n'
+XML_HEAD = '<?xml version="1.0" encoding="ASCII"?>\n'
 
 
 def bundle_xml(entry_src, entry_type, entry_id, entry_title, entry_date, entry_desc):
     """
     Bundles the data in xml.
     """
-    document = lb.E.document(
-        lb.E.title(entry_title),
-        lb.E.date(entry_date),
-        lb.E.description(entry_desc),
+    document = lb.E.Document(
+        lb.E.Title(entry_title),
+        lb.E.Date(entry_date),
+        lb.E.Description(entry_desc),
         src=entry_src, id=entry_id, type=entry_type)		
     document_string = etree.tostring(document, pretty_print=True)
     return XML_HEAD + document_string
