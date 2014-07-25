42d27dad905f0d156e0d228bed5b4131bdd8e648
Manish Chavan <manishc1@umbc.edu>
2014-07-25 14:36:24 -0400

    Changes.

--- src/similarity/logistic_regression.py
+++ src/similarity/logistic_regression.py
@@ -1,5 +1,18 @@
 print(__doc__)
 
+# ---------
+import sys
+import os
+
+COMMON_DIR = os.path.abspath(__file__ + '/../../common/')
+
+sys.path.insert(0, COMMON_DIR)
+from constants import *
+
+sys.path.insert(0, UTILS_DIR)
+from file_utils import *
+# ---------
+
 
 # Code source: Gael Varoqueux
 # Modified for Documentation merge by Jaques Grobler
@@ -9,22 +22,25 @@ import numpy as np
 import pylab as pl
 from sklearn import linear_model, datasets
 
+
+lines = read_to_lines('/home/manish/Dropbox/Thesis/MGSeCor/result/cve_cve/cross_site_scripting/from85_result.data', False)
+results = []
+for line in lines:
+	result = line.split(',')
+	result = [float(x.strip()) for x in result]
+	results.append(result)
+
+
 # import some data to play with
 #iris = datasets.load_iris()
 #X = iris.data[:, :2]  # we only take the first two features.
 #Y = iris.target
 
-X = np.array([[0.53193676, 0.5437775],
-		   [0.30260032, 0.2411011],
-		   [0.40098268, 0.37724027],
-		   [0.44290617, 0.46887624],
-		   [0.4473684, 0.4910687],
-		   [0.25891605, 0.2711118],
-		   [0.65022707, 0.6199698],
-		   [0.36583582, 0.3569689],
-		   [0.29456925, 0.3325378],
-		   [0.5239142, 0.52475464]])
-Y = np.array([0.5, 0.3, 0.4, 0.6, 0.6, 0.2, 0.6, 0.2, 0.4, 0.6])
+X = np.array([x[0:2] for x in results][0:75])
+Y = np.array([x[2] for x in results][0:75])
+
+X_test = np.array([x[0:2] for x in results][75:])
+Y_test = np.array([x[2] for x in results][75:])
 
 h = .02  # step size in the mesh
 
@@ -33,9 +49,30 @@ logreg = linear_model.LogisticRegression(C=1e5)
 # we create an instance of Neighbours Classifier and fit the data.
 logreg.fit(X, Y)
 print '**************************'
+print 'Weight Vector'
 print logreg.coef_
 print '**************************'
+print 'Actual:     ', ['%.2f' % z for z in Y_test.tolist()]
 
+error = 0
+errors = []
+for i in range(len(Y_test.tolist())):
+	error = error + ((Y_test.tolist()[i]-logreg.predict(X_test).tolist()[i])**2)
+	errors.append((Y_test.tolist()[i]-logreg.predict(X_test).tolist()[i])**2),
+print list(sorted(errors))
+print 'Combination:', ['%.2f' % z for z in logreg.predict(X_test).tolist()], (error/len(Y_test.tolist()))**0.5
+
+error = 0
+for i in range(len(Y_test.tolist())):
+	error = error + ((Y_test.tolist()[i]-[x[0] for x in results][75:][i])**2)
+print 'Secure:     ', ['%.2f' % z for z in [x[0] for x in results][75:]], (error/len(Y_test.tolist()))**0.5
+
+error = 0
+for i in range(len(Y_test.tolist())):
+	error = error + ((Y_test.tolist()[i]-[x[1] for x in results][75:][i])**2)
+print 'Generic:    ', ['%.2f' % z for z in [x[1] for x in results][75:]], (error/len(Y_test.tolist()))**0.5
+print '**************************'
+"""
 # Plot the decision boundary. For that, we will assign a color to each
 # point in the mesh [x_min, m_max]x[y_min, y_max].
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
@@ -59,3 +96,4 @@ pl.xticks(())
 pl.yticks(())
 
 pl.show()
+"""
